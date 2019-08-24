f27e646b9b7bf9a5bd3210756a8cf2cdd9bdb393
GunelHus <guneloh@code.edu.az>
2019-08-24 10:15:30 +0400

    1st task added

---
+++ HomeTask/HomeTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aralarinda boshluq simvolu olmaqla ededler daxil edin");
+            List<int> listChange = new List<int>();
+            string[] numbers =  Console.ReadLine().Split(' ');
+            foreach (var change in numbers) {
+                int intChange = int.Parse(change);
+                listChange.Add(intChange);
+            }
+            Console.WriteLine(listChange.Max());
+            Console.WriteLine(listChange.Sum());
+        }  
+    }
+}
